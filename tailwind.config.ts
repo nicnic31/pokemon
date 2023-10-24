c490dbd680a6c5950689f5f7f64fda6b98b1d1f9
nicnic31 <permejonics@gmail.com>
2023-10-24 23:39:44 +0800

    configured the colors of tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/layout/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        yellow: "#F4CE14",
+        "dark-yellow": "#E9B824",
+        gray: "#7D7C7C",
+        "dark-gray": "#61677A",
+        blue: "#00A9FF",
+        "dark-blue": "#337CCF",
+        red: "#FF6969",
+        "dark-red": "#D80032",
+        green: "#748E63",
+        "dark-green": "#445D48",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
