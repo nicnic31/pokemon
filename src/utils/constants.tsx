1a4d50fc83245f4f3bc867643e8c301759ac09d1
nicnic31 <permejonics@gmail.com>
2023-10-27 02:21:07 +0800

    created util folder and constant file

---
+++ src/utils/constants.tsx
@@ -0,0 +1,39 @@
+import BugIcon from "@/components/icon/pokemonIcon/bug";
+import DarkIcon from "@/components/icon/pokemonIcon/dark";
+import DragonIcon from "@/components/icon/pokemonIcon/dragon";
+import ElectricIcon from "@/components/icon/pokemonIcon/electric";
+import FairyIcon from "@/components/icon/pokemonIcon/fairy";
+import FightingIcon from "@/components/icon/pokemonIcon/fighting";
+import FireIcon from "@/components/icon/pokemonIcon/fire";
+import FlyingIcon from "@/components/icon/pokemonIcon/flying";
+import GhostIcon from "@/components/icon/pokemonIcon/ghost";
+import GlassIcon from "@/components/icon/pokemonIcon/glass";
+import GroundIcon from "@/components/icon/pokemonIcon/ground";
+import IceIcon from "@/components/icon/pokemonIcon/ice";
+import NormalIcon from "@/components/icon/pokemonIcon/normal";
+import PoisonIcon from "@/components/icon/pokemonIcon/poison";
+import PsychicIcon from "@/components/icon/pokemonIcon/psychic";
+import RockIcon from "@/components/icon/pokemonIcon/rock";
+import SteelIcon from "@/components/icon/pokemonIcon/steel";
+import WaterIcon from "@/components/icon/pokemonIcon/water";
+
+export const pokemonTypes = [
+  { label: "Bug", icon: <BugIcon /> },
+  { label: "Dark", icon: <DarkIcon /> },
+  { label: "Dragon", icon: <DragonIcon /> },
+  { label: "Electric", icon: <ElectricIcon /> },
+  { label: "Normal", icon: <NormalIcon /> },
+  { label: "Rock", icon: <RockIcon /> },
+  { label: "Fairy", icon: <FairyIcon /> },
+  { label: "Fighting", icon: <FightingIcon /> },
+  { label: "Fire", icon: <FireIcon /> },
+  { label: "Flying", icon: <FlyingIcon /> },
+  { label: "Poison", icon: <PoisonIcon /> },
+  { label: "Steel", icon: <SteelIcon /> },
+  { label: "Ghost", icon: <GhostIcon /> },
+  { label: "Glass", icon: <GlassIcon /> },
+  { label: "Ground", icon: <GroundIcon /> },
+  { label: "Ice", icon: <IceIcon /> },
+  { label: "Pyschic", icon: <PsychicIcon /> },
+  { label: "Water", icon: <WaterIcon /> },
+];
