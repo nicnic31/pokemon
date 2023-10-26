7286434c2e1e96f5a0bc642da65fd375056f0f7d
nicnic31 <permejonics@gmail.com>
2023-10-27 00:50:22 +0800

    added filter button for types

--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -4,21 +4,31 @@ import Layout from "@/layout";
 import { GET_POKEMONS } from "@/queries/pokemons";
 import { useQueryData } from "@/hooks/useQueryData";
 import Search from "@/components/search";
+import Button from "@/components/ui/button";
+import FilterIcon from "@/components/icon/filter";
 
 export default function Pokedex() {
   const [variables, setVariables] = useState({ first: 20 });
-  const [name, setName] = useState<string>("")
+  const [name, setName] = useState<string>("");
   const { data } = useQueryData("pokemons", GET_POKEMONS, { ...variables });
 
   console.log("data", data);
   return (
     <Layout>
-      <div className="flex flex-row">
-        <Search
-          placeholder="Search by name..."
-          value={name}
-          handleSearch={(search: string) => setName(search)}
-        />
+      <div className="flex flex-row align-items gap-2">
+        <div className="w-full">
+          <Search
+            placeholder="Search by name..."
+            value={name}
+            handleSearch={(search: string) => setName(search)}
+          />
+        </div>
+
+        <div>
+          <Button className="h-full px-3">
+            <FilterIcon />
+          </Button>
+        </div>
       </div>
     </Layout>
   );
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: ILayoutProps) {
   return (
     <div className="w-full font-sans">
       <Navbar />
-      <div className="mt-[90px] h-[88vh] pt-5 pb-2 px-4 sm:px-3 md:px-12 lg:px-24">
+      <div className="mt-[90px] h-[88vh] pt-5 pb-2 px-3 xs:px-1 sm:px-2 md:px-4 lg:px-24">
         {children}
       </div>
     </div>
